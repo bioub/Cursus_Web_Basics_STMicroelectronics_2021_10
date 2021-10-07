3d29f2b1429cd36250ad87b53c4cc463b6966c7c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-10-07 17:15:22 +0200

    Debut JS

---
+++ JavaScript/01-hello.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// déclaration de variable (globale) -> A NE JAMAIS UTILISER (erreur en mode strict)
+// maVar = 'maValeur';
+
+// déclaration de variable (portée locale à une fonction) -> A NE JAMAIS UTILISER
+// var prenom = 'Romain';
+
+// déclaration de variable (portée locale à un bloc)
+let prenom = 'Romain'; // on peut réaffecter une nouvelle valeur (prenom = 'Toto')
+const nom = 'Bohdanowicz'; // à privilégier
+
+// avec const l'objet reste modifiable (la référence affectée à la variable non)
+const names = ['Jean', 'Paul', 'Eric', 123];
+names.push('Jacques');
+
+function hello(name) {
+  return `Hello ${name} !`; // template literal
+}
+
+for (const n of names) {
+  console.log(hello(n));
+}
---
+++ JavaScript/02-compilation-jit.js
@@ -0,0 +1,61 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, '2');
+console.timeEnd('sum');
+
+console.time('sum');
+sum(1, 2);
+console.timeEnd('sum');
+
+/*
+sum: 0.118ms
+sum: 0.012ms
+sum: 0.005ms
+sum: 0.004ms
+sum: 0.005ms
+sum: 0.009ms
+sum: 0.002ms
+sum: 0.006ms
+sum: 0.002ms
+sum: 0.062ms
+sum: 0.004ms
+*/
