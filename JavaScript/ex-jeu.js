a0bdd3ee8af0b67ae545e2e5c8656e3d57840aff
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-10-18 13:59:46 +0200

    4 fonctions

--- JavaScript/ex-jeu.js
+++ JavaScript/ex-jeu.js
@@ -2,8 +2,20 @@ function getRandom() {
   return Math.random();
 }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
 // importe l'API readline de Node.js
