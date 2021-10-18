0b4fa8d0c6c53355ebc947e1053b45804c88747c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-10-18 16:53:19 +0200

    Correction ES6

---
+++ JavaScript/ex-jeu-es6+.js
@@ -0,0 +1,70 @@
+const Random = {
+  getRandom() {
+    return Math.random();
+  },
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+  },
+}
+
+const readline = require('readline');
+
+class Jeu {
+  constructor(options = {}) {
+    const { min = 0, max = 100 } = options;
+
+    // const min = options.min ?? 0;
+    // const max = options.max ?? 100;
+
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    this.entierAlea = Random.getRandomInt(min, max);
+    this.essais = [];
+  }
+  jouer() {
+    if (this.essais.length) {
+      console.log(`Vous avez déjà joué : ${this.essais.join(' - ')}!`);
+    }
+
+    this.rl.question('Quel est le nombre entier ? ', (answer) => {
+      console.log('Vous avez saisi : ' + answer);
+
+      const entierSaisi = Number.parseInt(answer, 10);
+
+      if (Number.isNaN(entierSaisi)) {
+        console.log('Erreur : il faut saisir un entier');
+        return this.jouer();
+      }
+
+      this.essais.push(entierSaisi);
+
+      if (entierSaisi < this.entierAlea) {
+        console.log('Trop petit');
+        this.jouer();
+      } else if (entierSaisi > this.entierAlea) {
+        console.log('Trop grand');
+        this.jouer();
+      } else {
+        console.log('Gagné');
+        this.rl.close();
+      }
+    });
+  }
+}
+
+
+const jeu = new Jeu();
+jeu.jouer();
